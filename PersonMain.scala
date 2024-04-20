d82499076e45a9e18ddd08be26ae69670054fddd
idiattara <74354003+idiattara@users.noreply.github.com>
2024-04-20 14:24:42 +0200

    Update PersonMain.scala

--- PersonMain.scala
+++ PersonMain.scala
@@ -1,8 +1,12 @@
 object PersonMain {
   def main(args: Array[String]): Unit = {
-val etudiant=new Etudiant("diop",23,"uvs")
-val a=etudiant.manger()
+    val etudiant=new Etudiant("diop",23,"uvs")
     println(etudiant.presentation())
-    println(a)
+    println(etudiant.manger())
+    etudiant.checkmaturity()
+    val sal=new Salarie("fall",34,2000,1200)
+    println(sal.presentation())
+    println(sal.manger())
+    sal.checkmaturity()
   }
 }
