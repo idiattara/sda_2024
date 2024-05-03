1dc236dc0dcfa875bb7a240007f4144bf241db78
idiattara <74354003+idiattara@users.noreply.github.com>
2024-05-03 17:37:42 +0200

    Create ConfigurationParser.scala

---
+++ ConfigurationParser.scala
@@ -0,0 +1,29 @@
+package sda.parser
+
+import org.json4s.DefaultFormats
+import org.json4s.jackson.JsonMethods
+import sda.reader._
+
+object ConfigurationParser {
+
+  implicit val format = DefaultFormats
+
+  def getCsvReaderConfigurationFromJson(jsonString: String): CsvReader = {
+    JsonMethods.parse(FileReaderUsingIOSource.getContent(jsonString)).extract[CsvReader]
+  }
+
+  /* Complétez cette fonction. Elle prend un String  en argument et renvoie un objet JsonReader.
+     Elle doit être codée de la même manière que la fonction  getCsvReaderConfigurationFromJson
+
+  def getJsonReaderConfigurationFromJson(jsonString: String): JsonReader = {
+  ................................................................
+  }
+   */
+
+
+  def getJsonReaderConfigurationFromJson(jsonString: String): JsonReader = {
+    JsonMethods.parse(FileReaderUsingIOSource.getContent(jsonString)).extract[JsonReader]
+  }
+}
+
+
